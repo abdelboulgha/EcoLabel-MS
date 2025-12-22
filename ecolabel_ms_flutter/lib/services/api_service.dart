4c473f50e96618c5918ccaf46af2d9e260758dad
ahmed-elhamri <elhamri949@gmail.com>
2025-12-22 12:37:09 +0100

    ..

--- ecolabel_ms_flutter/lib/services/api_service.dart
+++ ecolabel_ms_flutter/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import '../models/product.dart';
 
 class ApiService {
   // Utilisez votre IP Wi-Fi
-  static const String baseUrl = 'http://192.168.1.12:8080/PARSER-PRODUIT/product/parse-from-image';
+  static const String baseUrl = 'http://192.168.1.12:8080/PARSER-PRODUIT/';
   
   Future<ProductParseResponse> parseProduct({
     required String barcode,
